0b78b61b8fb170e21fc61982a21081ddf9fd7a94
szirmaigy <szirm.gyozo@gmail.com>
2019-11-17 08:18:51 +0100

    base code

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+# from waitress import serve
+import uuid, sys, os, re
+
+app = Flask(__name__)
+
+
+@app.after_request
+def add_hostname_header(response):
+    env_host = str(os.environ.get('HOSTNAME'))
+    hostname = re.findall('[a-z]{3}-\d$', env_host)
+    if hostname:
+            response.headers["SP-LOCATION"] = hostname
+    return response
+
+@app.route('/')
+def homepage():
+    try:
+        return render_template("index.html")
+    except Exception as e:
+	    return render_template("500.html", error = str(e))
+
+@app.route('/uuid', methods=['POST'])
+def get_uuid():
+    try:
+        uuid_p = str(uuid.uuid4())
+        return render_template("index.html", uuid=uuid_p)
+    except Exception as e:
+	    return render_template("500.html", error = str(e))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+if __name__ == "__main__":
+    serve(app, listen='*:8081')
